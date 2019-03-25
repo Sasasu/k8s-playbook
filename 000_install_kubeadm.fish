e01da607c26abdd42482eabd48b663345266aafa
Sasasu <su@sasasu.me>
2019-03-25 11:24:43 +0800

    fix: add node-ip to kubeadm.conf

--- 000_install_kubeadm.fish
+++ 000_install_kubeadm.fish
@@ -4,7 +4,7 @@ wget "https://github.com/containernetworking/plugins/releases/download/$CNI_VERS
 wget "https://github.com/kubernetes-incubator/cri-tools/releases/download/$CRICTL_VERSION/crictl-$CRICTL_VERSION-linux-amd64.tar.gz"
 wget https://storage.googleapis.com/kubernetes-release/release/$RELEASE/bin/linux/amd64/{kubeadm,kubelet,kubectl}
 curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/$RELEASE/build/debs/kubelet.service" > kubelet.service
-curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/$RELEASE/build/debs/10-kubeadm.conf"  > 10-kubeadm.conf
+curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/$RELEASE/build/debs/10-kubeadm.conf" > 10-kubeadm.conf
 
 for i in $ALL_VM
     echo doing $i
@@ -18,7 +18,7 @@ for i in $ALL_VM
     end
 
     cat kubelet.service | ssh $i sudo sh -c "\"cat > /etc/systemd/system/kubelet.service\""
-    cat 10-kubeadm.conf | ssh $i sudo sh -c "\"cat > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf\""
+    cat 10-kubeadm.conf | sed "s:/usr/bin/kubelet:/usr/bin/kubelet --node-ip=$i:g" | ssh $i sudo sh -c "\"cat > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf\""
     ssh $i sudo "systemctl enable --now kubelet"
     echo 'export PATH="$PATH:/opt/bin"' | ssh $i "cat >> ~/.bashrc"
 end
